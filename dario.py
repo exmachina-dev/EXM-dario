45f9bff49f0c37f0d801c1f11dcc9762d6c86417
Adrian Badoil <adrian.badoil@exmachina.fr>
2018-01-08 10:12:25 +0100

    Rename QmainWindow name, add file menu and edit fct

--- dario.py
+++ dario.py
@@ -1,7 +1,7 @@
 import sys
 from functools import partial
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox, QMdiSubWindow, QWidget
 from PyQt5.QtWidgets import QPushButton, QComboBox, QSpinBox, QDoubleSpinBox, QLineEdit
 from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsObject
 from PyQt5.QtCore import pyqtSlot, pyqtSignal, QTimer
@@ -11,7 +11,9 @@ import PyQt5.uic as uic
 import time
 import os.path
 
-class TestConfig(QMainWindow):
+VERSION = '0.1'
+
+class Dario(QMainWindow):
     UIFILE = '.\dario.ui'
 
     def __init__(self):
@@ -20,18 +22,46 @@ class TestConfig(QMainWindow):
         self.initUi()
 
     def initUi(self):
-        self.setWindowTitle('Test Config Tool')
+        self.setWindowTitle('Dario')
 
         _path = os.path.dirname(os.path.realpath(__file__))
         self.main = uic.loadUi(_path + self.UIFILE)
 
         self.setCentralWidget(self.main)
+
+        fileMenu = self.menuBar().addMenu('File')
+
+        quitAction = fileMenu.addAction('Quit')
+        quitAction.triggered.connect(self.doQuit)
+
+        fileMenu = self.menuBar().addMenu('Tools')
+        connexionAction = fileMenu.addAction('Connexion')
+        connexionAction.triggered.connect(self.doConnexion)
+
+        aboutAction = self.menuBar().addAction('About')
+        aboutAction.triggered.connect(self.doAbout)
+
+
         self.show()
+
+    def doQuit (self):
+        QApplication.quit()
+
+    def doAbout(self):
+        about = QMessageBox.information(self, 'About',
+        '''Dario - Version: v''' + str(VERSION),
+        QMessageBox.Ok)
+
+    def doConnexion(self):
+
+    def doQuit(self):
+        self.windows2 = None
+        QWidget.quit()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    w = TestConfig()
+    w = Dario()
 
     sys.exit(app.exec_())
 
