61604efe7ba54e2fa33bde4735df6ca50ac254e1
Adrian Badoil <adrian.badoil@exmachina.fr>
2018-01-08 17:56:28 +0100

    Add log handler and cmd_line fcts

--- dario.py
+++ dario.py
@@ -1,14 +1,14 @@
 import sys
 from functools import partial
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox, QMdiSubWindow, QWidget
-from PyQt5.QtWidgets import QPushButton, QComboBox, QSpinBox, QDoubleSpinBox, QLineEdit
-from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsObject
-from PyQt5.QtCore import pyqtSlot, pyqtSignal, QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QWidget
+from PyQt5.QtWidgets import QTextEdit, QLineEdit
+from PyQt5.QtGui import QColor, QTextCursor
 
 import PyQt5.uic as uic
 
-import time
+import logging as lg
+
 import os.path
 
 VERSION = '0.1'
@@ -29,6 +29,21 @@ class Dario(QMainWindow):
 
         self.setCentralWidget(self.main)
 
+        self._menuBar()
+
+        self.log_list = self.main.findChild(QTextEdit,'log_list')
+        self.cmd_line = self.main.findChild(QLineEdit, 'cmd_line')
+        self.embedded_log_handler = EmbeddedLogHandler(self.log_list)
+        self.embedded_log_handler.setFormatter(embedded_formatter)
+        logging.addHandler(self.embedded_log_handler)
+        self.embedded_log_handler.setLevel(lg.DEBUG)
+        logging.setLevel(lg.DEBUG)
+
+        self.show()
+
+        self.cmd_line.returnPressed.connect(self._cmd_send)
+
+    def _menuBar(self):
         fileMenu = self.menuBar().addMenu('File')
 
         quitAction = fileMenu.addAction('Quit')
@@ -39,16 +54,57 @@ class Dario(QMainWindow):
         aboutAction = self.menuBar().addAction('About')
         aboutAction.triggered.connect(self.doAbout)
 
-        self.show()
-
     def doQuit (self):
         # Options save method may be called here
         QApplication.quit()
 
     def doAbout(self):
         about = QMessageBox.information(self, 'About',
-        '''Dario - Version: v''' + str(VERSION),
-        QMessageBox.Ok)
+                '''Dario - Version: v''' + str(VERSION),
+                QMessageBox.Ok)
+
+    def _cmd_send(self):
+        self.cmd_line.clear()
+
+
+class EmbeddedLogHandler(lg.Handler):
+
+    def __init__(self, widget):
+        self.setLevel(lg.DEBUG)
+
+        self.widget = widget
+
+        self.color = {
+                "INFO": QColor(127, 254, 127),
+                "DEBUG": QColor(254, 254, 254),
+                "WARNING": QColor(254, 254, 127),
+                "ERROR": QColor(254, 127,127),
+                "CRITICAL": QColor(254, 0, 0),
+                }
+        super().__init__()
+
+    def emit(self, record):
+        msg = '<pre style="color: {}; display: inline-block;">{}</pre><br>'.format(
+                self.color[record.levelname].name(), self.format(record))
+
+        self.widget.moveCursor(QTextCursor.End)
+        self.widget.insertHtml(msg)
+
+
+console_logger = lg.StreamHandler()
+console_formatter = lg.Formatter(
+        '%(asctime)s %(name)-36s %(levelname)-8s %(message)s',
+        datefmt='%Y%m%d %H:%M:%S')
+
+embedded_formatter = lg.Formatter(
+        '%(asctime)s %(name)-10s %(levelname)-8s %(message)s',
+        datefmt='%H:%M:%S')
+
+logging = lg.getLogger('dario-gui')
+logging.addHandler(console_logger)
+console_logger.setFormatter(console_formatter)
+
+logging.setLevel(lg.DEBUG)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
